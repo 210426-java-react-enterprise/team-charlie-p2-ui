91ea02f0c9a8bb51afcfe9580ce004be6e882a5e
Austin <austinknauer@alumni.colostate.edu>
2021-06-05 21:22:31 -0600

    Implemented menu functiooinality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Menu from "./components/Menu"
 import { React, useState } from 'react';
 
 function App() {
 
   const [screen, setScreen] = useState("home");
+  const [homepage, setHomepage] = useState("home");
+  const [menu, setMenu] = useState(false);
+  const [menuOptions, setMenuOptions] = useState(["Login", "Register"]);
 
   const viewChange = (e) => {
     const newScreen = e.target.getAttribute("data-route");
     setScreen(newScreen);
   }
 
+  const toggleMenu = () => {
+    setMenu(!menu);
+  }
+
   return (
     <div className="App">
+      {menu && <Menu menuOptions={menuOptions} viewChange={viewChange} />}
+      <nav id="nav-bar">
+        <button type="button" id="home-button" data-route={homepage} onClick={viewChange}>_HOME</button>
+        <button type="button" id="menu-button" onClick={toggleMenu}>_MENU</button>
+      </nav>
       {screen === "home" && <Home viewChange={viewChange} />}
       {screen === "login" && <Login viewChange={viewChange} />}
       {screen === "register" && <Register viewChange={viewChange} />}
