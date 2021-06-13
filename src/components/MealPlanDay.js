74fb82e5c9268e1e81be1b0c6f0eb9d4e0ce385c
ozzy castillo <oswaldo.castillo@revature.net>
2021-06-13 13:59:34 -0500

    MealPLanDay - infinite loop fixed

--- src/components/MealPlanDay.js
+++ src/components/MealPlanDay.js
@@ -9,19 +9,20 @@ import Button  from 'react-bootstrap/Button';
 export default function MealPlanDay(props){
 
     const[mealTimeRows, setMealTimeRows] = useState([])
-
-    let count=0;
+    const[count, setCount] = useState(0);
+    
 
     function addMealTime(){
-        count++;
-        console.log(count);
         const recipeSelectName = "recipeSelect"
         let recipeSelectId = recipeSelectName.concat(count);
         const optionValueName = "RecipeFav";
         let optionValue = optionValueName.concat(count);
-
+        // if(count < 0){
+        //     let dif= 0 - count;
+        //     setCount(count => count + dif);
+        // }
         const row =(
-            <Row class="mealTime">
+            <Row key={count} class="mealTime">
             <Col>
                 <label class="mealTimeLbl" for={recipeSelectId}>Meal Time # {count}</label>
             </Col> 
@@ -33,8 +34,18 @@ export default function MealPlanDay(props){
             </Col> 
         </Row>
         )
-        
+        setCount(count => count + 1);
+        console.log(count);
         setMealTimeRows(mealTimeRows => [...mealTimeRows, row])
+    }
+
+    
+
+    function removeMealTime(){
+            const newMealTimeRow = mealTimeRows.filter(Row => Row.key != count);
+            setMealTimeRows(newMealTimeRow);
+            setCount(() => count - 1); 
+            console.log(count);
     }
     
     return(
@@ -48,7 +59,10 @@ export default function MealPlanDay(props){
                         <Button variant="light" onClick={() => addMealTime()} style={{ marginLeft: `${50}%`}}>Add Meal Time</Button>
                     </Col>
                     <Col>
-                        <Button variant="light">Save</Button>
+                        <Button className= "btn" variant="light" onClick={() => removeMealTime()} style={{ marginLeft: `${20}%`}}>Remove Meal Time</Button>
+                    </Col>
+                    <Col>
+                        <Button className= "btn" variant="light">Save</Button>
                     </Col>
                 </Row>
             </div>
