da8710c70df2e958fef788020e7e0da3d660c2ae
Austin <austinknauer@alumni.colostate.edu>
2021-06-13 16:32:43 -0600

    restyled nav bar and menu

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
 
   // useState("home") returns ["home", function for updating state]
   // needs state to hold favorite recipes (probably an array of objects) and meal plan (probably an object of objects)
-  const [screen, setScreen] = useState("register");
-  const [landing, setLanding] = useState(true);
+  const [screen, setScreen] = useState("settings");
+  const [landing, setLanding] = useState(false);
   const [menu, setMenu] = useState(false);
   const [menuOptions, setMenuOptions] = useState(["Dashboard", "Search", "Favorites", "Meal Plan", "Settings"]);
   const [favorites, setFavorites] = useState([]);
